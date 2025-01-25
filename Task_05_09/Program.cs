1e8ed9a8ca1391ab11ce1427c7563274981949d3
Артём Конопатый <dipresharik@outlook.com>
2025-01-26 01:23:52 +0500

    Выполнена 9 задача 5 урока

---
+++ Task_05_09/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Task_05_09
+{
+    internal class Program
+    {
+        /*
+        Дан квадратный массив размерность n*n. Произведите анализ данной матрицы и выясните является ли данная матрица Z-матрицей
+        (это матрица, где все недиагональные элементы меньше нуля)
+        Если данное условие выполняется то вывести данную матрицу с цветовой индикацией главной диагонали. Если не выполняется, то
+        вывести сообщение что данная матрица не является Z-матрицей 
+        */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размерность матрицы");
+            int n = int.Parse(Console.ReadLine());
+
+            Random rnd = new Random();
+            int[,] matric = new int[n, n];  // матрица
+            bool isZMatric = true;          // переменная проверки Z-матрицы
+
+            // заполение матрицы
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                {
+                    matric[i, j] = rnd.Next(-10, 11);
+
+                    // проверка на Z-матрицу
+                    if (i != j && i != n - j - 1 && matric[i, j] >= 0)
+                        isZMatric = false;
+                }
+
+            // вывод
+            if (isZMatric == false)
+                Console.WriteLine("Матрица не является Z-матрицей!");
+            else
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (i == j)
+                        {
+                            Console.BackgroundColor = ConsoleColor.Green;
+                            Console.ForegroundColor = ConsoleColor.DarkBlue;
+                        }
+
+                        Console.Write($"{matric[i, j]} ");
+                        Console.BackgroundColor= ConsoleColor.Black;
+                        Console.ForegroundColor= ConsoleColor.White;
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
