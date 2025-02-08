292a2346e502a3c67ac259a5de0040635736fc61
Артём Конопатый <dipresharik@outlook.com>
2025-02-08 20:19:01 +0500

    Выполнена 6 задача 10 урока

---
+++ Task_10_06/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Task_10_06
+{
+    internal class Program
+    {
+        /* 
+         * Создать Метод ArrayGeneration не возвращающий значения, 
+         * принимает целое число n, 
+         * выводит на консоль сгенерированный массив размерности n*n.
+         */
+        static void Main(string[] args)
+        {
+            Console.Write("Введите размерность квадратной матрицы: ");
+            int n = int.Parse(Console.ReadLine());      // размерность матрицы
+
+            ArrayGeneration(n);
+        }
+
+        /// <summary>
+        /// Генерирует матрицу случайных чисел размером n * n
+        /// </summary>
+        /// <param name="n"> размерность матрицы </param>
+        static void ArrayGeneration(int n)
+        {
+            Random rnd = new Random();
+            int[,] nums = new int[n, n];    // матрица
+
+            Console.WriteLine("\nСгенерированнный массив:");
+            // генерация и вывод
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    nums[i, j] = rnd.Next(10, 100);
+                    Console.Write($"{nums[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
