1af6c20a8605f1e0f130eb32781b984e91656a35
Артём Конопатый <dipresharik@outlook.com>
2025-02-19 15:15:24 +0500

    Выполнена 1 задача 12 урока

---
+++ Task_12_01/Publications/Journal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_12_01.Publications
+{
+    internal class Journal
+    {
+        public string date;
+        public string theme;
+        public string title;
+        public int countOfPages;
+        public int price;
+        private bool isBuy = false; // куплен ли журнал
+
+        public void BuyJournal()
+        {
+            if (isBuy) Console.WriteLine($"Журнал: {title} уже приобретён");
+            else
+            {
+                isBuy = true;
+                Console.WriteLine($"Вы приобрели журнал: {title}, выпущенный: {date} за {price}");
+            }
+        }
+
+        public void ReadingJournal()
+        {
+            Console.WriteLine($"Вы прочитали журнал:{title} \nВыпуска: {date} \nТематика:{theme} \nОбъёмом {countOfPages} страниц");
+        }
+    }
+}
---
+++ Task_12_01/Publications/Newspaper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_12_01.Publications
+{
+    internal class Newspaper
+    {
+        public string date;
+        public string header;
+        public string title;
+        public int countOfPages;
+        public int price;
+
+        public void getInfo()
+        {
+            Console.WriteLine("Создан экземпляр класса Newapaper");
+            Console.WriteLine($"Дата издания: {date} \nзаголовок: {header} \nназвание: {title} \nкол-во страниц: {countOfPages} \nцена: {price}");
+        }
+    }
+}
