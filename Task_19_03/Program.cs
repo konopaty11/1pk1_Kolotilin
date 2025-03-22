6ad03fd3a75fc16612b29bb715897742cdafa419
Артём Конопатый <dipresharik@outlook.com>
2025-03-22 18:41:13 +0500

    Выполнена 3 задача 19 урока

---
+++ Task_19_03/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Task_19_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string resStr = "";
+
+            int i = 1;  // счётчик строк
+            while (true)
+            {
+                Console.Write($"Введите строку {i++} >> ");
+                string str = Console.ReadLine();
+
+                if (str == "") break;   // выход из цикла
+
+                resStr = string.Join('-', new string[] { resStr, str });    // формирование результирующей строки
+            }
+
+            Console.WriteLine(resStr.Trim('-'));
+        }
+    }
+}
