e910b54596f231af879efba0fb30ae83cc33b52b
Артём Конопатый <dipresharik@outlook.com>
2025-03-22 18:27:44 +0500

    Выполнена 2 задача 19 урока

---
+++ task_19_02/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace task_19_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст:");
+            string text = Console.ReadLine();
+
+            Console.WriteLine("Текст разделённый на слова:");
+            foreach (string word in text.Split())
+                Console.WriteLine(word.Trim());
+
+            Console.WriteLine("\nТекст разделённый на предложения:");
+            foreach (string sentence in text.Split(new char[3] { '.', '!', '?' }))
+                Console.WriteLine(sentence.Trim());
+        }
+    }
+}
