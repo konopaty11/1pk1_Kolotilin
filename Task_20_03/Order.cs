d743550afb3c82757a41d3cf2d552dec744f0e7a
Артём Конопатый <dipresharik@outlook.com>
2025-03-25 21:42:13 +0500

    Выполнена 3 задача 20 урока

---
+++ Task_20_03/Order.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_20_03
+{
+    enum StatusOrder
+    {
+        New,
+        Processing,
+        Shipped,
+        Delivered,
+        Cancelled,
+    }
+
+    class Order
+    {
+        public StatusOrder Status { get; private set; } = StatusOrder.New;
+
+        public void ChangeStatus()
+        {
+            if (Status != StatusOrder.Delivered && Status != StatusOrder.Cancelled) Status++;
+            else Console.WriteLine("Заказ уже был доставлен или отменён");
+        }
+
+        public void CancelledOrder()
+        {
+            if (Status != StatusOrder.Cancelled) Status = StatusOrder.Cancelled;
+            else Console.WriteLine("Данный заказ уже был отменён");
+        }
+    }
+}
---
+++ Task_20_03/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Task_20_03
+{
+    internal class Program
+    {
+        /*
+         * Создайте программу для управления статусом заказа в интернет-магазине. Используйте перечислениеOrderStatus со значениями:
+            • New (новый)
+            • Processing (в обработке)
+            • Shipped (отправлен)
+            • Delivered (доставлен)
+            • Cancelled (отменён)
+            Напишите метод, который меняет статус заказа. Если заказ уже доставлен или отменён, запретите изменение статуса. 
+            Выводите сообщение при каждом изменении статуса (например,
+            "Заказ переведён в статус: Отправлен").
+         */
+        static void Main(string[] args)
+        {
+            Order order1 = new Order();
+            Console.WriteLine($"Заказ переведён в статус: {order1.Status}");
+
+            order1.ChangeStatus();
+            Console.WriteLine($"Заказ переведён в статус: {order1.Status}");
+
+            order1.ChangeStatus();
+            Console.WriteLine($"Заказ переведён в статус: {order1.Status}");
+
+            order1.ChangeStatus();
+            Console.WriteLine($"Заказ переведён в статус: {order1.Status}");
+
+            order1.ChangeStatus();
+
+
+            Order order2 = new Order();
+            Console.WriteLine($"\nЗаказ переведён в статус: {order2.Status}");
+
+            order2.CancelledOrder();
+            Console.WriteLine($"Заказ переведён в статус: {order2.Status}");
+            order2.ChangeStatus();
+            order2.CancelledOrder();
+        }
+    }
+}
