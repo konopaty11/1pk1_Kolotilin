b273e06ead4e9beadddf3613edf3168ac3a2320c
Артём Конопатый <dipresharik@outlook.com>
2025-03-25 21:09:41 +0500

    Реализован алгоритм выявления победителя в задаче 2 урока 20

---
+++ Task_20_02/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Task_20_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Results(Enum.Rock, Enum.Paper);
+        }
+
+        /// <summary>
+        /// вычисляет результат раунда
+        /// </summary>
+        /// <param name="compChoose"> выбор компьютера </param>
+        /// <param name="userChoose"> выбор юзера </param>
+        static void Results(Enum compChoose, Enum userChoose)
+        {
+            /* 
+             * преобразование разницы числового представления 
+             * выборов компьютера и пользователя
+             * из значений -2, 2 в 1, -1 соответственно
+            */
+            int res = (int)compChoose - (int)userChoose;
+            if (res % 2 == 0) res = -(res / 2);
+
+            // итоговое вычисление результата
+            if (res == 1) Console.WriteLine("юзер победил");
+            else if (res == -1) Console.WriteLine("комп победил");
+            else if (res == 0) Console.WriteLine("ничья");
+        }
+    }
+}
