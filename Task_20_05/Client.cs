b2331806fc92ebdcca823293253e4a0ca61ffd05
Артём Конопатый <dipresharik@outlook.com>
2025-03-25 22:57:25 +0500

    Выполнена 5 задача 20 урока

---
+++ Task_20_05/Client.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_20_05
+{
+    enum AccessLevel
+    {
+        Guest,
+        User,
+        Moderator,
+        Admin
+    }
+
+    class Client
+    {
+        AccessLevel client;
+
+        public Client(AccessLevel client)
+        {
+            this.client = client;
+        }
+
+        /// <summary>
+        /// чтение для всех кроме модератора
+        /// </summary>
+        public void Read()
+        {
+            if ((int)client != (int)AccessLevel.Moderator) Console.WriteLine($"{client} читает...");
+            else Console.WriteLine($"{client} не имеет столько прав");
+        }
+
+        /// <summary>
+        /// Создание контента только для пользователя и админа
+        /// </summary>
+        public void CreateContent()
+        {
+            if ((int)client >= (int)AccessLevel.User && (int)client != (int)AccessLevel.Moderator) Console.WriteLine($"{client} создаёт контент...");
+            else Console.WriteLine($"{client} не имеет столько прав");
+        }
+
+        /// <summary>
+        /// Удаление контента только для модератора и админа
+        /// </summary>
+        public void DeleteContent()
+        {
+            if ((int)client >= (int)AccessLevel.Moderator) Console.WriteLine($"{client} удаляет контент...");
+            else Console.WriteLine($"{client} не имеет столько прав");
+        }
+    }
+}
---
+++ Task_20_05/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Task_20_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Client guest = new Client(AccessLevel.Guest);
+            guest.Read();
+            guest.DeleteContent();
+
+            Client user = new Client(AccessLevel.User);
+            user.CreateContent();
+            user.Read();
+
+            Client moderator = new Client(AccessLevel.Moderator);
+            moderator.CreateContent();
+            moderator.DeleteContent();
+
+            Client admin = new Client(AccessLevel.Admin);
+            admin.Read();
+            admin.DeleteContent();
+        }
+    }
+}
